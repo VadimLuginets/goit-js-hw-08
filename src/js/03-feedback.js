5e572b6c0fad3b2dc7709b32b2044f8e78638428
Vadim luginets <pebeeo@gmail.com>
2022-08-18 19:01:43 +0300

    3dTask
    works

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
+const FORM_DATA_OBJECT = `feedback-form-state`;
 
+const formEl = document.querySelector('form');
+const formSubmitBtn = document.querySelector('button');
+
+const savedFormDataObject = localStorage.getItem(FORM_DATA_OBJECT);
+const parsedFormDataObject = JSON.parse(savedFormDataObject);
+
+console.log(parsedFormDataObject);
+
+formEl.email.value = parsedFormDataObject.email;
+formEl.message.value = parsedFormDataObject.message;
+
+formEl.addEventListener(
+  'input',
+  throttle(event => {
+    event.preventDefault();
+    const dataToSave = {
+      email: formEl.email.value,
+      message: formEl.message.value,
+    };
+
+    localStorage.setItem(FORM_DATA_OBJECT, JSON.stringify(dataToSave));
+
+    showLogs();
+  }, 500)
+);
+
+formSubmitBtn.addEventListener('click', event => {
+  event.preventDefault();
+  const dataToSave = {
+    email: formEl.email.value,
+    message: formEl.message.value,
+  };
+  dataToSave.email = '';
+  dataToSave.message = '';
+  localStorage.setItem(FORM_DATA_OBJECT, JSON.stringify(dataToSave));
+  const savedFormDataObject = localStorage.getItem(FORM_DATA_OBJECT);
+  const parsedFormDataObject = JSON.parse(savedFormDataObject);
+
+  formEl.email.value = parsedFormDataObject.email;
+  formEl.message.value = parsedFormDataObject.message;
+
+  showLogs();
+});
+
+function showLogs() {
+  console.log(`email= ${formEl.email.value}`);
+  console.log(`message= ${formEl.message.value}`);
+}
